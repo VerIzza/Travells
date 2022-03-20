0825a04364eaad733e216eb40c0cc181d49d639c
EBADILLO030423 <90670128+EBADILLO030423@users.noreply.github.com>
2022-03-20 03:10:20 -0600

    quite mamaaaaa

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -28,7 +28,6 @@ public class Steps {
 		driver.manage().window().maximize();
 		wait = new WebDriverWait(this.driver, 10);
 		util = new Util();
-		System.out.println("mamaaa ohh uuuhhhhh i dont wanna cry");
 	}
 		
 	@Test
